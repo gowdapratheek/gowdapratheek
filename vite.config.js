660bce68912188170105afce5477d57f2ae392d1
Pratheek Gowda <gowdapratheek91@gmail.com>
2024-11-01 01:00:35 +0530

    host

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0", // Listen on all network interfaces
-    port: 4173, // Ensure this is the port you are using
-  },
   plugins: [react()],
 });
